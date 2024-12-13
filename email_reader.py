f263b3c76e9c2080964b7dadb873c4346fd16f33
marcelizzi <marcelizzi@gmail.com>
2024-12-13 15:20:04 -0300

    V0.1

--- email_reader.py
+++ email_reader.py
@@ -14,7 +14,7 @@ with IMAPClient(HOST) as server:
     print(server.list_folders())
     server.select_folder("INBOX",readonly=True)
     #Pesquisa
-    server.search([u"SINCE",date(datetime.now().year,datetime.now().month,datetime.now().day)])
+    messages = server.search([u"SINCE",date(datetime.now().year,datetime.now().month,datetime.now().day)])
 
     for uid, message_data in server.fetch(messages,'RFC822').items():
         email_message = email.message_from_bytes(message_data[b'RFC822'])
